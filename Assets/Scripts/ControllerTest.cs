a62141aa83a6778820c177618d397256660be68d
Gin-home <jaron.95@hotmail.com>
2018-02-12 23:46:10 +0100

    - Initial setup commit.

---
+++ Assets/Scripts/ControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire1");
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Debug.Log("Fire2");
+        }
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            Debug.Log("Horizontal");
+        }
+    }
+}
