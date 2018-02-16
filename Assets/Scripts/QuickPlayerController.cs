c1d8335b9a6ebd70039327f1cb191e6c32547166
Gin <jaron.95@hotmail.com>
2018-02-16 17:00:57 +0100

    Rope tests

---
+++ Assets/Scripts/QuickPlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickPlayerController : MonoBehaviour {
+
+    Vector2 move;
+    Rigidbody rb;
+
+    public float speed;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        
+        if (Input.GetAxis("Left Stick X 1") != 0) {
+            move.x = Input.GetAxis("Left Stick X 1");
+        }
+
+        if (Input.GetAxis("Left Stick Y 1") != 0) {
+            move.y = Input.GetAxis("Left Stick Y 1");
+        }
+    }
+
+    private void FixedUpdate() {
+        //print(move);
+        transform.Translate(new Vector3(move.x * speed, 0, move.y * speed), Space.World);
+        //rb.AddForce(new Vector3(move.x * speed, 0, move.y * speed), ForceMode.Impulse);
+
+    }
+}
---
+++ Assets/Scripts/QuickPlayerKeyboardController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickPlayerKeyboardController : MonoBehaviour {
+
+    Vector2 move;
+    Rigidbody rb;
+
+    public float speed;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+
+        if (Input.GetAxis("Horizontal") != 0) {
+            move.x = Input.GetAxis("Horizontal");
+        }
+
+        if (Input.GetAxis("Vertical") != 0) {
+            move.y = Input.GetAxis("Vertical");
+        }
+    }
+
+    private void FixedUpdate() {
+        //print(move);
+        transform.Translate(new Vector3(move.x * speed, 0, move.y * speed),Space.World);
+        //rb.AddForce(new Vector3(move.x * speed, 0, move.y * speed), ForceMode.Impulse);
+
+    }
+}
