cbf1ef4e215b6b9dbd66ca5b7ff0d415621e5d51
Gin-home <jaron.95@hotmail.com>
2018-02-17 00:08:14 +0100

    Ready for small mechanic prototype

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float time;
+    public Vector3 cameraOffset;
+    
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(transform.position, target.position + cameraOffset, time);
+	}
+}
