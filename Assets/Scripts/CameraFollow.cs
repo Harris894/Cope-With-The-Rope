75bbfd5d965e6fb05409e3789f86ea117337b57c
Gin <jaron.95@hotmail.com>
2018-03-15 10:08:03 +0100

    Camera changed

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
-    public Transform target;
+    public List<Transform> targets;
     public float time;
     public Vector3 cameraOffset;
     
 	// Update is called once per frame
 	void Update () {
-        transform.position = Vector3.Lerp(transform.position, target.position + cameraOffset, time);
+        Vector3 targetTransform = Vector3.zero;
+        foreach(Transform target in targets){
+            targetTransform += target.transform.position;
+        }
+        targetTransform = targetTransform / targets.Count;
+        
+        transform.position = Vector3.Lerp(transform.position, targetTransform + cameraOffset, time);
+        transform.LookAt(targetTransform);
 	}
 }
