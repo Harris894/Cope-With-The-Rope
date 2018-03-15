6a45ca6ec1dbd576ae222c9dd838674328375d70
Harris894 <x.arisakritidis@hotmail.com>
2018-03-15 20:23:11 +0100

    Harris' fucked up version

---
+++ Assets/ChangeCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour {
+
+    public Camera mainCamera;
+    public Camera insideCamera;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            mainCamera.gameObject.SetActive(false);
+            insideCamera.gameObject.SetActive(true);
+        }
+    }
+}
