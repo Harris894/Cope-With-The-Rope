031b64fc0d6f885166aa49a19980c0d84b5abc2f
Harris894 <x.arisakritidis@hotmail.com>
2018-04-16 15:15:01 +0200

    bla

---
+++ Assets/Scripts/Animation/FuckingTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuckingTest : MonoBehaviour {
+
+    public GameObject otherCube;
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.DrawRay(transform.position, otherCube.transform.position - transform.position, Color.green);
+	}
+}
