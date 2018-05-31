439dab396eacf839bd118322da7368e59e7abf45
Gin-home <jaron.95@hotmail.com>
2018-05-31 03:47:45 +0200

    Pro player controller basic

--- Assets/Scripts/Player/ProPlayerController.cs
+++ Assets/Scripts/Player/ProPlayerController.cs
@@ -8,14 +8,21 @@ public class ProPlayerController : MonoBehaviour {
 
     [Header("Settings")]
     public bool moveRelativeToCamera;
+    public float groundCheckRayExtendDistance;
 
     [Header("Physics")]
     public float speed;
     public float jumpPower;
+    public float jumpCooldown;
+    public float airMovementPenalty;
     [Range(0,4)]
     public float gravityMultiplier;
     public LayerMask groundLayers;
 
+    [Header("Debug")]
+    [SerializeField]
+    private bool inAir;
+    private float lastJumpTime;
     private bool canMove;
     private Vector3 move;
 
@@ -24,6 +31,8 @@ public class ProPlayerController : MonoBehaviour {
     [SerializeField] Transform cam;
     Rigidbody rb;
     ProUserInput proUserInput;
+    Collider characterCollider;
+    Vector3 lastMoveDirection;
 
 
     //Input
@@ -39,31 +48,68 @@ public class ProPlayerController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
+        characterCollider = GetComponent<Collider>();
         proUserInput = GetComponent<ProUserInput>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+
+        //Apply the movement
+        if (moveRelativeToCamera) {
+            if (cam == null) {
+                cam = Camera.main.transform;
+            }
+            Vector3 camDirection = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
+            move = (i_moveForward * camDirection * speed + i_moveRight * Camera.main.transform.right * speed) * Time.deltaTime;
+        }
+        else {
+            //Set movement values
+            move.z = i_moveForward * speed;
+            move.x = i_moveRight * speed;
+        }
+
+        //Add extra gravity for the player.
         rb.velocity += Physics.gravity * gravityMultiplier;
 
-        if(i_jump > 0) {
+        //Groundcheck
+        Debug.DrawRay(characterCollider.bounds.center,
+            Vector3.down * (characterCollider.bounds.extents.y + groundCheckRayExtendDistance), Color.red);
+        if (Physics.Raycast(characterCollider.bounds.center,
+            Vector3.down * (characterCollider.bounds.extents.y + groundCheckRayExtendDistance),2f,groundLayers)) {
+            inAir = false;
+            Debug.Log("Hit ground");
+        }
+        else {
+            inAir = true;
+        }
+
+        //front hit test
+        RaycastHit hit;
+        Debug.DrawRay(characterCollider.bounds.center, transform.forward, Color.magenta);
+        if(Physics.Raycast(characterCollider.bounds.center,transform.forward, out hit, 10f, groundLayers)) {
+            //Debug.Log(hit.transform.name);
+        }
+        
+
+        //Jumping
+        if (i_jump > 0 && !inAir && Time.time > lastJumpTime + jumpCooldown) {
             rb.velocity = new Vector3(rb.velocity.x, jumpPower, rb.velocity.z);
         }
-
-        //if(i_moveForward != 0) {
-
-        //}
-
-        //if(i_moveRight != 0) {
-        //    //vecto
-        //}
-        move.z = i_moveForward * speed;
-        move.x = i_moveRight * speed;
-        if (moveRelativeToCamera) {
-            Vector3 camDirection = Vector3.Scale(cam.forward, new Vector3(1, 0, 1)).normalized;
-            move = i_moveForward * camDirection * speed + i_moveRight * Camera.main.transform.right * speed;
+        //Air nudge
+        else if(inAir && lastJumpTime - jumpCooldown > Time.time) {
+            rb.AddForce(move, ForceMode.Impulse);
         }
 
-        rb.MovePosition(transform.position + move);
+        //Apply the movement to the object
+        if (!inAir) {
+            rb.MovePosition(transform.position + move);
+            lastMoveDirection = move;
+        }
+        else {
+            //rb.MovePosition(transform.position + lastMoveDirection);
+            rb.MovePosition(transform.position + lastMoveDirection + (move * airMovementPenalty));
+        }
+
     }
 }
--- Assets/Scripts/Player/ProUserInput.cs
+++ Assets/Scripts/Player/ProUserInput.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class ProUserInput : MonoBehaviour {
 
     public int playerNumber;
-    public bool useJoystick;
 
     ProPlayerController proPlayerController;
 
     //Inputs
-    float moveFoward, MoveRight;
+    float moveForward, moveRight;
     float jump;
 
 	// Use this for initialization
@@ -20,25 +19,33 @@ public class ProUserInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (!useJoystick) {
-            #region Keyboard
-            //jump = Input.GetAxis()
-            jump = Input.GetAxis(string.Format("Primary Action{0}", playerNumber));
-            moveFoward = Input.GetAxis(string.Format("Vertical{0}", playerNumber));
-            MoveRight = Input.GetAxis(string.Format("Horizontal{0}", playerNumber));
-            #endregion
-        }
-        else {
-            #region Joystick
-            jump = Input.GetAxis(string.Format("A{0}", playerNumber));
-            moveFoward = Input.GetAxis(string.Format("Left Stick Y {0}", playerNumber));
-            MoveRight = Input.GetAxis(string.Format("Left Stick X {0}", playerNumber));
-            #endregion
-        }
+
+
+        //Reset values back to 0 so that it doesn't stack up.
+        jump = 0;
+        moveForward = 0;
+        moveRight = 0;
+
+        #region Keyboard
+        jump += Input.GetAxis(string.Format("Primary Action{0}", playerNumber));
+        moveForward += Input.GetAxis(string.Format("Vertical{0}", playerNumber));
+        moveRight += Input.GetAxis(string.Format("Horizontal{0}", playerNumber));
+        #endregion
+
+        #region Joystick
+        jump += Input.GetAxis(string.Format("A{0}", playerNumber));
+        moveForward += Input.GetAxis(string.Format("Left Stick Y {0}", playerNumber));
+        moveRight += Input.GetAxis(string.Format("Left Stick X {0}", playerNumber));
+        #endregion
+
+        //Limit all input between -1 and 1 since we are summing up all the values from different input methods.
+        jump = Mathf.Clamp(jump, -1, 1);
+        moveForward = Mathf.Clamp(moveForward, -1, 1);
+        moveRight = Mathf.Clamp(moveRight, -1, 1);
 
         //Debug.Log(jump);
-        //Debug.Log(moveFoward);
+        //Debug.LogFormat("Forward: {0} + Right: {1}", moveForward, moveRight);
         //Debug.Log(Input.GetJoystickNames().Length);
-        proPlayerController.SendInput(jump, moveFoward, MoveRight);
+        proPlayerController.SendInput(jump, moveForward, moveRight);
     }
 }
