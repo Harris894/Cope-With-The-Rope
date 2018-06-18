856597f693741c483dd1c6e08e161a473694141c
Harris894 <x.arisakritidis@hotmail.com>
2018-06-18 17:28:57 +0200

    Latest One

---
+++ Assets/Scripts/DoorProgression.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorProgression : MonoBehaviour {
+
+ 
+    public void DoorTriggered()
+    {
+        ProgressionManager.instance.AddProgressPointValue();
+    }
+
+
+}
